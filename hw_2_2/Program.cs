6262e780f825831aacf5a5d61961b2d9f4f9643b
illidan <berkovskiilia@gmail.com>
2022-09-05 22:48:21 +0300

    hw_2

---
+++ hw_2_2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите трехзначное число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2){
+  Console.WriteLine($"третья цифра -> {NumberText[2]}");
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
---
+++ hw_2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите порядковый номер дня недели: ");
+
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+void ChecDayWeek (int dayWeek) {
+    if (dayWeek == 6 || dayWeek == 7) {
+    Console.WriteLine("(этот день выходной) -> да");
+    }
+    else if (dayWeek < 1 || dayWeek > 7) {
+        Console.WriteLine("такого дня недели не существует ");
+    }
+    else Console.WriteLine("(этот день выходной) -> нет");
+}
+
+ChecDayWeek(dayWeek);
