57d6068e62b18b04bb6a966bd5cf124309447f8e
illidan <berkovskiilia@gmail.com>
2022-09-10 22:15:32 +0300

    Домашнее 3

---
+++ hw_3_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void CheckNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Число: {number} - является палиндром.");
+  }
+  else Console.WriteLine($"Число: {number} - не является палиндром.");
+}
+
+if (number!.Length == 5){
+  CheckNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
---
+++ hw_3_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int x1 = Coordinat("x", "A");
+int y1 = Coordinat("y", "A");
+int z1 = Coordinat("z", "A");
+int x2 = Coordinat("x", "B");
+int y2 = Coordinat("y", "B");
+int z2 = Coordinat("z", "B");
+
+int Coordinat(string NameCoor, string NamePoint)
+{
+    Console.Write($"Введите координату {NameCoor} точки {NamePoint}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double Length =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {Length}");
+
